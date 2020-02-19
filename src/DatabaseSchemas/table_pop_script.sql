266bf7ead578e55bb5734e17c2059dd019a31b0a
iamyobany <44537895+iamyobany@users.noreply.github.com>
2020-02-19 16:31:48 -0500

    Category scripts written

---
+++ src/DatabaseSchemas/table_pop_script.sql
@@ -0,0 +1,10 @@
+use article_db;
+
+insert into categories (category_name, category_description) values
+	("Academic", "Academic related articles"),
+    ("Research", "Research articles"),
+    ("Professions", "Job listings for caretakers"),
+    ("Symptoms", "Articles covering ASD symptoms"),
+    ("Treatments", "Articles covering ASD treatments"),
+    ("Charities", "Information pertaining to ASD charities");
+    
