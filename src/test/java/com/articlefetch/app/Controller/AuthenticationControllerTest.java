7bd16d18942332f741cbe85661e79ffd36fb622d
Joshua Schappel <jmschappel12@gmail.com>
2020-03-17 14:51:54 -0400

    Finished authentication tests

--- src/test/java/com/articlefetch/app/Controller/AuthenticationControllerTest.java
+++ src/test/java/com/articlefetch/app/Controller/AuthenticationControllerTest.java
@@ -1,5 +1,7 @@
 package com.articlefetch.app.Controller;
 
+import com.articlefetch.app.Busniess.Exceptions.AccountNotFoundException;
+import com.articlefetch.app.Busniess.Exceptions.InvalidPasswordException;
 import com.articlefetch.app.Busniess.Service.LoginService;
 import com.articlefetch.app.Controller.JacksonModels.Account;
 import com.articlefetch.app.Controller.JacksonModels.Authentication;
@@ -47,6 +49,32 @@ class AuthenticationControllerTest {
                 .andExpect(jsonPath("$['id']", is(12)))
                 .andExpect(jsonPath("$['path']", IsNull.nullValue()))
                 .andExpect(jsonPath("$['status']", is(true)));
+    }
+
+    @Test
+    void login_with_invalid_password() throws Exception {
+        Authentication validation = new Authentication("jschappel", "password2");
+
+        when(service.validateAccount(validation.getUsername(), validation.getPassword()))
+                .thenThrow(new InvalidPasswordException(validation.getUsername()));
+
+        mvc.perform(post("/authenticate/login")
+                .contentType(APPLICATION_JSON)
+                .content(asJsonString(validation)))
+                .andExpect(jsonPath("$['message']", is("Invalid password for jschappel")));
+    }
+
+    @Test
+    void login_with_invalid_username() throws Exception {
+        Authentication validation = new Authentication("jschappel", "password2");
+
+        when(service.validateAccount(validation.getUsername(), validation.getPassword()))
+                .thenThrow(new AccountNotFoundException(validation.getUsername()));
+
+        mvc.perform(post("/authenticate/login")
+                .contentType(APPLICATION_JSON)
+                .content(asJsonString(validation)))
+                .andExpect(jsonPath("$['message']", is("Username: jschappel does not exist")));
     }
 
     // Helper function that converts a Jackson Object to a json string
