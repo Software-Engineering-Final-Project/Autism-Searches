926517cb8fc21968f08f1b932cdfe2083c860374
Joshua Schappel <jmschappel12@gmail.com>
2020-03-20 15:21:44 -0400

    Added more categories to the database

--- src/DatabaseSchemas/table_pop_script.sql
+++ src/DatabaseSchemas/table_pop_script.sql
@@ -1,10 +1,17 @@
 use article_db;
 
 insert into categories (category_name, category_description) values
+	("Education", "Schools for individuals with ASD"),
+    ("Theropy", "Theropists and leading theropy techinques"),
+    ("Medical", "Latest medical advancments"),
 	("Academic", "Academic related articles"),
     ("Research", "Research articles"),
     ("Professions", "Job listings for caretakers"),
     ("Symptoms", "Articles covering ASD symptoms"),
     ("Treatments", "Articles covering ASD treatments"),
+    ("News Articles", "News Atricles that realte to ASD"),
+    ("Technology", "The latest technological advancements in ASD"),
+	("Supprot Groups", "Support groups for individuals with ASD"),
     ("Charities", "Information pertaining to ASD charities");
     
+    SELECT * FROM categories;
